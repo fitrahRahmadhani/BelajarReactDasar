5e44f14e18259ba4666b543612a08488706658e7
fitrahRahmadhani <2131710092@student.polinema.ac.id>
2024-02-29 07:45:25 +0700

    Relearn ReactJS

---
+++ WPU/my-form/src/MyForm.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+
+class MyForm extends React.Component{
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            name: '',
+            email: '',
+            gender: 'Man'
+        };
+
+        this.onNameChangeEventHandler = this.onNameChangeEventHandler.bind(this);
+        this.onEmailChangeEventHandler = this.onEmailChangeEventHandler.bind(this);
+        this.onGenderChangeEventHandler = this.onGenderChangeEventHandler.bind(this);
+        this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
+    }
+
+    onNameChangeEventHandler(event){
+        this.setState(() => {
+            return {
+                name: event.target.value
+            }
+        })
+    }
+    onEmailChangeEventHandler(event){
+        this.setState(() => {
+            return {
+                email: event.target.value
+            }
+        })
+    }
+    onGenderChangeEventHandler(event){
+        this.setState(() => {
+            return {
+                gender: event.target.value
+            }
+        })
+    }
+    onSubmitEventHandler(event){
+        event.preventDefault();
+
+        const message = `
+            Name: ${this.state.name}
+            Email: ${this.state.email}
+            Gender: ${this.state.gender}
+        `;
+
+        alert(message);
+    }
+
+
+
+    render(){
+        return (
+            <div>
+                <h1>Register Form</h1>
+                <form action="" onSubmit={this.onSubmitEventHandler}>
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" id="name" value={this.state.name} onChange={this.onNameChangeEventHandler}/>
+                    <br />
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" id="email" value={this.state.email} onChange={this.onEmailChangeEventHandler}/>
+                    <br />
+                    <label htmlFor="gender">Gender:</label>
+                    <select name="gender" id="gender" value={this.state.gender} onChange={this.onGenderChangeEventHandler}>
+                        <option value="Man">Man</option>
+                        <option value="Woman">Woman</option>
+                    </select>
+                    <br />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<MyForm />)
